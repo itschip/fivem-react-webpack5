7e8adc2a0b755f371f47b3eb37902ca2511b288d
Christopher <onlygamernerd@gmail.com>
2021-04-22 13:29:47 +0200

    inital commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className='App'>
+      <h1>Hello FiveM</h1>
+    </div>
+  )
+}
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index.bundle.js'
+  },
+  devServer: {
+    port: 300,
+    watchContentBase: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+}
