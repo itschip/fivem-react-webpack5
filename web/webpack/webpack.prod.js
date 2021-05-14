b27583b0a3576bafc19d09b8954579c47f8e2cc1
Christopher <onlygamernerd@gmail.com>
2021-05-14 20:50:31 +0200

    fix compiling css

--- web/webpack/webpack.prod.js
+++ web/webpack/webpack.prod.js
@@ -1,62 +1,59 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    path: path.join(__dirname, '../dist'),
-    filename: 'index.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true
-          }
-        }
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.css$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              outputPath: 'css',
-              name: 'app.css'
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '../dist'),
+        filename: 'index.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        transpileOnly: true
+                    }
+                }
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin, 'css-loader'],
+                include: /node_modules/,
             }
-          },
-          "style-loader",
-          "css-loader",
         ]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      minify: {
-        minifyCSS: true,
-        minifyJS: true,
-        minifyURLs: true,
-        removeComments: true,
-        removeRedundantAttributes: true,
-        collapseWhitespace: true,
-        removeEmptyAttributes: true, 
-      },
-      inject: true
-    })
-  ],
-  resolve: { extensions: [".tsx", ".ts", ".js", ".jsx" ] },
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            minify: {
+                minifyCSS: true,
+                minifyJS: true,
+                minifyURLs: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                collapseWhitespace: true,
+                removeEmptyAttributes: true,
+            },
+            inject: true
+        }),
+        new MiniCssExtractPlugin()
+    ],
+    resolve: { extensions: [".tsx", ".ts", ".js", ".jsx"] },
 }
